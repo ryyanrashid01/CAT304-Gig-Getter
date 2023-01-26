f5219c6279cead61afc0d1785c0608531b0f8ae8
Xaipes <97590245+Xaipes@users.noreply.github.com>
2023-01-26 12:28:38 +0800

    Version 2.0

--- Gig_Post.js
+++ Gig_Post.js
@@ -1,6 +1,30 @@
+//Setup MySQL2
+const mysql = require('mysql2/promise');
+
+//Temp
+var gig_index = 2;
+
+async function Gig_Post(){
+
+    //Connect to database
+    const connection = await mysql.createConnection({
+        host: 'sql12.freesqldatabase.com',
+        user: 'sql12592913',
+        database: 'sql12592913',
+        password: 'VCmJjDvxQy'
+    });
+
+    const [rows, fields] = await connection.execute('SELECT * FROM `Gig_Post` WHERE `gigPostID` = ?', [2]);
+    var Gig_Title = rows[0].gigTitle;
+    var Gig_ID = rows[0].gigPostID;
+    console.log(rows[0]);
+};
+
+
+
 //Create variables to store the post's bid information
-var Minimum_Bid = 40.0;
+var Minimum_Bid = 2.0;
 var Current_Bid = 60.0;
 var User_Bid = 0.0;
 
-console.log(Current_Bid);
+Gig_Post();
